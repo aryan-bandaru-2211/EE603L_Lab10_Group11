dc42ad657911819f0df69d1dae43372e9e075398
shenlong <220020009@iitdh.ac.in>
2025-10-21 13:06:42 +0530

    Stack overflow implemented

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include "tm4c123gh6pm.h"
 #include "stdint.h"
-
+//Inducing stack overflow by increasing number of volatile variables and increasing number of sums to count
 
 int series_sum(int);
 
 int main()
 {
     int sum,k;
-    k = 10; //Calculating sum of first 10
+    k = 100; //Calculating sum of first 100
 
     sum = series_sum(k);
     return 1;
@@ -16,7 +16,7 @@ int main()
 int series_sum(int k)
 {
     //Declaring 10 variables
-    volatile int a,b,c,d,e,f,g,h,i,j;
+    volatile int a= 1,b = 1,c = 1,d = 1,e = 1;
     if (k == 1)
     {
         return 1;
